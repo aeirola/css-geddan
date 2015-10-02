f69ad15ae806f82028eb2456ffcae48271186d0b
Axel Eirola <axel.eirola@futurice.com>
2015-10-02 08:52:47 +0300

    Initial commit

---
+++ public/geddan.js
@@ -0,0 +1,18 @@
+function random() {
+  return Math.random() * 2 - 1;
+}
+
+function geddanify() {
+  var geddans = document.getElementsByClassName('geddan');
+
+  window.setInterval(function geddanStep() {
+    for(var i = 0; i < geddans.length; i++) {
+      var element = geddans.item(i);
+      var transform = 'matrix(' + random() + ',' + random() + ',' + random() + ',' + random() + ',' + random() + ',' + random() + ')';
+      element.style.webkitTransform = transform;
+    }
+  }, 20);
+
+}
+
+window.onload = geddanify;
