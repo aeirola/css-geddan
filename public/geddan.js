ec6e5ecf8c6838c6d9149a88088d41fa4b2946f1
Axel Eirola <axel.eirola@futurice.com>
2015-10-06 15:49:57 +0300

    Add possibility for custom image

--- public/geddan.js
+++ public/geddan.js
@@ -2,8 +2,25 @@ function random() {
   return Math.random() * 2 - 1;
 }
 
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 function geddanify() {
   var geddans = document.getElementsByClassName('geddan');
+
+  var imgUrl = getParameterByName('img');
+  for(var i = 0; i < geddans.length; i++) {
+    var element = geddans.item(i);
+    if (imgUrl) {
+      element.style.backgroundImage = 'url(' + imgUrl + ')';
+    } else {
+      element.style.backgroundColor = 'black';
+    }
+  }
 
   window.setInterval(function geddanStep() {
     for(var i = 0; i < geddans.length; i++) {
